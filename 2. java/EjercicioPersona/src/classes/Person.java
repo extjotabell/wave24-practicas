5b34885168bb7f50dbd1228b135fb4a1091349ac
victoriaborquezcarm <victoria.borquezcarmona@mercadolibre.cl>
2024-01-02 12:24:36 -0300

    [FEAT] Modify Person class
    Add methods calculateIMC, isOlder and toString

--- 2. java/EjercicioPersona/src/classes/Person.java
+++ 2. java/EjercicioPersona/src/classes/Person.java
@@ -26,4 +26,36 @@ public class Person {
         this.weight = weight;
         this.height = height;
     }
+
+    public int calculateIMC(){
+        double imc = weight/(Math.pow(height,2));
+        if(imc < 20){
+            return -1;
+        }
+        else if (imc >= 20 && imc <= 25){
+            return 0;
+        } else  {
+            return 1;
+        }
+
+    }
+
+    public boolean isOlder(){
+        if(age >= 18){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", dni='" + dni + '\'' +
+                ", weight=" + weight +
+                ", height=" + height +
+                '}';
+    }
 }
