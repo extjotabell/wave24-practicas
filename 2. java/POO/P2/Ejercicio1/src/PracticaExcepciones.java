3ed4485b1202f487fbab5a25965583a4f6cae662
saenzJuan <juan.saenzgarcia@mercadolibre.com.co>
2024-01-02 18:05:13 -0500

    [FEAT] Exercise POO

---
+++ 2. java/POO/P2/Ejercicio1/src/PracticaExcepciones.java
@@ -0,0 +1,39 @@
+public class PracticaExcepciones {
+    int a = 0;
+    int b = 300;
+
+    public PracticaExcepciones(){
+        primeraOpc();
+        segundaOpc();
+    }
+
+    public int calcularCociente(int numerador, int denominador) {
+        return numerador / denominador;
+    }
+    
+    ////////////////////////////////
+    public void primeraOpc(){
+        try {
+            int cociente = calcularCociente(b, a);
+            System.out.println("El cociente es: " + cociente);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Se ha producido un error");
+
+        } finally {
+            System.out.println("Programa finalizado");
+        }
+    }
+
+    /////////////////////////////////////////
+    public void segundaOpc(){
+        try {
+            int cociente = calcularCociente(b, a);
+            System.out.println("El cociente es: " + cociente);
+
+        } catch (ArithmeticException e) {
+            throw new IllegalArgumentException("No se puede dividir por cero");
+        }
+    }
+
+}
