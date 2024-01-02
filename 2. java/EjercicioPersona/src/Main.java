417f0e06290a17ab0bdf713475f39dee87de696c
victoriaborquezcarm <victoria.borquezcarmona@mercadolibre.cl>
2024-01-02 12:10:13 -0300

    [FEAT] Modify Main
    Person class objects are instantiated

--- 2. java/EjercicioPersona/src/Main.java
+++ 2. java/EjercicioPersona/src/Main.java
@@ -1,5 +1,9 @@
+import classes.Person;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Person personNull = new Person();
+        Person personSimple = new Person("Gatito", 14, "123456789-k");
+        Person personFull = new Person("Gatito", 14, "123456789-k", 6, 0.5);
     }
 }
