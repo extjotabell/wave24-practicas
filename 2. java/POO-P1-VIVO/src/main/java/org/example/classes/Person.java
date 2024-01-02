d3bd069f1640d645353c642567b5e8376b9d7895
ImanolSuppoMELI <imanol.suppo@mercadolibre.com>
2024-01-02 12:30:12 -0300

    [feat] Complete exercise 2

--- 2. java/POO-P1-VIVO/src/main/java/org/example/classes/Person.java
+++ 2. java/POO-P1-VIVO/src/main/java/org/example/classes/Person.java
@@ -7,4 +7,19 @@ public class Person {
     double weight;
     double height;
 
+    public  Person() {}
+
+    public Person(String name, int age, String dni) {
+        this.name = name;
+        this.age = age;
+        this.dni = dni;
+    }
+
+    public Person(String name, int age, String dni, double weight, double height) {
+        this.name = name;
+        this.age = age;
+        this.dni = dni;
+        this.weight = weight;
+        this.height = height;
+    }
 }
