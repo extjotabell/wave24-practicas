1b1b307f20c18ff9b4322f09a11ed06ea1f956bc
victoriaborquezcarm <victoria.borquezcarmona@mercadolibre.cl>
2024-01-02 12:31:13 -0300

    [FEAT] Modify Main class
    Add CalculateIMC, isOlder and print person

--- 2. java/EjercicioPersona/src/Main.java
+++ 2. java/EjercicioPersona/src/Main.java
@@ -4,6 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Person personNull = new Person();
         Person personSimple = new Person("Gatito", 14, "123456789-k");
-        Person personFull = new Person("Gatito", 14, "123456789-k", 6, 0.5);
+        Person personFull = new Person("Gatito", 15, "123456789-k", 54, 1.59);
+
+        System.out.println(personFull.toString());
+
+        if(personFull.calculateIMC() == -1){
+            System.out.println("La persona está bajo peso");
+        } else if (personFull.calculateIMC() == 0) {
+            System.out.println("La persona tiene peso saludable");
+        }else {
+            System.out.println("La persona está sobrepeso");
+        }
+
+        if(personFull.isOlder() == true){
+            System.out.println("La persona es mayor de edad");
+        }else{
+            System.out.println("La persona es menor de edad");
+        }
     }
+
+
 }
