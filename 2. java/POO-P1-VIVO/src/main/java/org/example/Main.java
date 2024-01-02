df82370f3f9c6281ba906ab41c672b4c461d647e
ImanolSuppoMELI <imanol.suppo@mercadolibre.com>
2024-01-02 12:35:38 -0300

    [feat] Complete exercise 4

--- 2. java/POO-P1-VIVO/src/main/java/org/example/Main.java
+++ 2. java/POO-P1-VIVO/src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.example.classes.Person;
+
 public class Main {
     public static void main(String[] args) {
+
+        Person PersonWithoutConstructor = new Person();
+
+        Person PersonAnyConstructor = new Person("Imanol", 20, "45084386");
+
+        Person PersonAllConstructor = new Person("Imanol", 20, "45084386", 80.2, 1.78);
+
 
     }
 }
