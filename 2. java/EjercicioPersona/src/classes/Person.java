5f3417f76d1ce87b3348a51ebf9561dd7d1c1584
victoriaborquezcarm <victoria.borquezcarmona@mercadolibre.cl>
2024-01-02 12:05:19 -0300

    [FEAT] Modify Person class
    Add constructors to Person class

--- 2. java/EjercicioPersona/src/classes/Person.java
+++ 2. java/EjercicioPersona/src/classes/Person.java
@@ -9,4 +9,21 @@ public class Person {
     int weight;
     double height;
 
+    //Constructors
+    public Person() {
+    }
+
+    public Person(String name, int age, String dni) {
+        this.name = name;
+        this.age = age;
+        this.dni = dni;
+    }
+
+    public Person(String name, int age, String dni, int weight, double height) {
+        this.name = name;
+        this.age = age;
+        this.dni = dni;
+        this.weight = weight;
+        this.height = height;
+    }
 }
