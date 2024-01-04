8a72712ce2d4ec4a2280ebd8322ccf37c30b25b8
Enzo Nicolas Comba <enzo.comba@mercadolibre.com>
2024-01-04 13:25:16 -0300

    [feat] added Ejercicio Integrador P1 VIVO Agencia Turismo

---
+++ 2. java/Integrador-Agencia-Turismo/src/classes/Boleto.java
@@ -0,0 +1,7 @@
+package classes;
+
+public class Boleto extends Reserva{
+    public Boleto(String nombre, Double precio) {
+        super(nombre,precio);
+    }
+}
---
+++ 2. java/Integrador-Agencia-Turismo/src/classes/ReservaHotel.java
@@ -0,0 +1,8 @@
+package classes;
+
+public class ReservaHotel extends Reserva {
+
+    public ReservaHotel(String nombre, Double precio) {
+        super(nombre,precio);
+    }
+}
