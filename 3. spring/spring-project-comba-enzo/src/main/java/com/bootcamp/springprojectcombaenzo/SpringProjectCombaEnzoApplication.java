a1e565e0e9b3b1ada201f5bd1c8907b6eb529037
Enzo Nicolas Comba <enzo.comba@mercadolibre.com>
2024-01-05 14:44:58 -0300

    [spring] added spring project

---
+++ 3. spring/spring-project-comba-enzo/src/main/java/com/bootcamp/springprojectcombaenzo/SpringProjectCombaEnzoApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.springprojectcombaenzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectCombaEnzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectCombaEnzoApplication.class, args);
+	}
+
+}
---
+++ 3. spring/spring-project-comba-enzo/src/test/java/com/bootcamp/springprojectcombaenzo/SpringProjectCombaEnzoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.springprojectcombaenzo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectCombaEnzoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
