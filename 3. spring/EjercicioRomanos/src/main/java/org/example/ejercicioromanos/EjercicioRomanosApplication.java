50bbf21d61a8f36f539c7748d78f0625511376e7
victoriaborquezcarm <victoria.borquezcarmona@mercadolibre.cl>
2024-01-05 15:38:28 -0300

    [FEAT] Project EjercicioRomanos

---
+++ 3. spring/EjercicioRomanos/src/main/java/org/example/ejercicioromanos/EjercicioRomanosApplication.java
@@ -0,0 +1,13 @@
+package org.example.ejercicioromanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioRomanosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioRomanosApplication.class, args);
+    }
+
+}
---
+++ 3. spring/EjercicioRomanos/src/test/java/org/example/ejercicioromanos/EjercicioRomanosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ejercicioromanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioRomanosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
