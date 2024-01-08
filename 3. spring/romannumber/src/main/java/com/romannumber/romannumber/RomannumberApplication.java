cf6d89a43f9e415b8c5f5b607e1cd87ba79df091
stagliaferri <stefanoguillermo.tagliaferri@mercadolibre.com>
2024-01-08 10:47:26 -0300

    ejercicio roman numbers

---
+++ 3. spring/romannumber/src/main/java/com/romannumber/romannumber/RomannumberApplication.java
@@ -0,0 +1,13 @@
+package com.romannumber.romannumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomannumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomannumberApplication.class, args);
+	}
+
+}
---
+++ 3. spring/romannumber/src/test/java/com/romannumber/romannumber/RomannumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romannumber.romannumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomannumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
