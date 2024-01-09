30ab98cff68d091e5b9e718587737cd4fff775b1
saenzJuan <juan.saenzgarcia@mercadolibre.com.co>
2024-01-09 14:43:36 -0500

    [TEST] test

---
+++ 2. java/recapitulandoJava/Ejercicio1/src/classes/Garaje.java
@@ -0,0 +1,27 @@
+package classes;
+import java.util.List;
+
+public class Garaje {
+
+    int id;
+    List<Vehiculo> listaVehiculos;
+    public Garaje(int id, List<Vehiculo> listaVehiculos) {
+        this.id = id;
+        this.listaVehiculos = listaVehiculos;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Vehiculo> getListaVehiculos() {
+        return listaVehiculos;
+    }
+
+    public void setListaVehiculos(List<Vehiculo> listaVehiculos) {
+        this.listaVehiculos = listaVehiculos;
+    }
+}
