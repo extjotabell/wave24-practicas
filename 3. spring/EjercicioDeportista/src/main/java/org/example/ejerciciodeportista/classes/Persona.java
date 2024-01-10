a53f493d346c657046293f1d428593ba642a9cd9
victoriaborquezcarm <victoria.borquezcarmona@mercadolibre.cl>
2024-01-10 09:57:31 -0300

    [FEAT] Add Persona class with attributes, constructor, getter and setter

---
+++ 3. spring/EjercicioDeportista/src/main/java/org/example/ejerciciodeportista/classes/Persona.java
@@ -0,0 +1,16 @@
+package org.example.ejerciciodeportista.classes;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Persona {
+    private  String nombre;
+    private String apellido;
+    private  Integer edad;
+}
+
+
