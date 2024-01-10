95c647d9c6c9297796d1792209aeb486daa61c82
DorisMELI <doriselena.salazar@mercadolibre.cl>
2024-01-09 22:32:48 -0300

    Add exerise sportExercise

---
+++ 3. spring/sportexercise/src/main/java/com/mercadolibre/sportexercise/classes/Sport.java
@@ -0,0 +1,15 @@
+package com.mercadolibre.sportexercise.classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Sport {
+    private String name;
+    private Integer level;
+}
