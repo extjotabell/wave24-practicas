83fa74c5e46b5b1c72af6203105d9adc72c66970
victoriaborquezcarm <victoria.borquezcarmona@mercadolibre.cl>
2024-01-10 09:59:13 -0300

    [FEAT] Add Deporte class with attributes, constructor, getter and setter

---
+++ 3. spring/EjercicioDeportista/src/main/java/org/example/ejerciciodeportista/classes/Deporte.java
@@ -0,0 +1,13 @@
+package org.example.ejerciciodeportista.classes;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Deporte {
+    private String nombreDeporte;
+    private String nivel;
+}
