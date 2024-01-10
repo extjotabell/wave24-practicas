d747dd5ec3ec117dc8fe74bc30557c76bb5a8396
jcguerrero02 <juancamilo.guerrero@mercadolibre.com.co>
2024-01-10 11:31:26 -0500

    Feat: adding Persona Deporte DTO practice

---
+++ 3. spring/DTO-Practica/src/test/java/org/example/dtopractica/DtoPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dtopractica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtoPracticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
