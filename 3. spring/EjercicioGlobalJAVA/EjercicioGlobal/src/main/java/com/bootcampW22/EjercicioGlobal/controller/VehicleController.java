b39c940a7747268e13377bccd31d6ada6f6bb9c8
stagliaferri <stefanoguillermo.tagliaferri@mercadolibre.com>
2024-01-12 15:10:13 -0300

    ejercicio de code review

---
+++ 3. spring/EjercicioGlobalJAVA/EjercicioGlobal/src/main/java/com/bootcampW22/EjercicioGlobal/controller/VehicleController.java
@@ -0,0 +1,32 @@
+package com.bootcampW22.EjercicioGlobal.controller;
+
+import com.bootcampW22.EjercicioGlobal.dto.VehicleDto;
+import com.bootcampW22.EjercicioGlobal.service.IVehicleService;
+import com.bootcampW22.EjercicioGlobal.service.VehicleServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class VehicleController {
+
+    IVehicleService vehicleService;
+
+    public VehicleController(VehicleServiceImpl vehicleService){
+        this.vehicleService = vehicleService;
+    }
+
+    @GetMapping("/vehicles")
+    public ResponseEntity<?> getVehicles(){
+        return new ResponseEntity<>(vehicleService.searchAllVehicles(), HttpStatus.OK);
+    }
+
+    @GetMapping("/vehicles/weight")
+    public ResponseEntity<List<VehicleDto>> getVehiclesByWeightRange(@RequestParam("weight_min") Double weight_min, @RequestParam("weight_max") Double weight_max){
+        return new ResponseEntity<>(vehicleService.getVehiclesByWeightRange(weight_min, weight_max), HttpStatus.OK);
+    }
+}
