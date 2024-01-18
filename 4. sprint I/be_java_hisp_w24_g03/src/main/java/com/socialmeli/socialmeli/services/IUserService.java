a149b5d062af3ca0564fde7248747f785b774940
victoriaborquezcarm <victoria.borquezcarmona@mercadolibre.cl>
2024-01-18 12:13:31 -0300

    [feat] Added group project.

---
+++ 4. sprint I/be_java_hisp_w24_g03/src/main/java/com/socialmeli/socialmeli/services/IUserService.java
@@ -0,0 +1,23 @@
+package com.socialmeli.socialmeli.services;
+
+import com.socialmeli.socialmeli.dto.ResponseDto;
+import com.socialmeli.socialmeli.dto.UserDto;
+import com.socialmeli.socialmeli.dto.UserFollowerDto;
+
+import java.util.List;
+
+import com.socialmeli.socialmeli.dto.UserFollowedDto;
+import com.socialmeli.socialmeli.dto.UserFollowersDto;
+import com.socialmeli.socialmeli.entities.User;
+
+import java.util.ArrayList;
+
+public interface IUserService {
+    UserFollowerDto getFollowers(Integer userId, String order);
+
+    List<UserDto> getAllUsers();
+    UserFollowersDto getTotalFollowers(Integer userId);
+    ResponseDto follow(Integer userId, Integer userIdToFollow);
+    ResponseDto unfollow(Integer userId, Integer userIdToFollow);
+    UserFollowedDto listFollowed(Integer userId, String order);
+}
