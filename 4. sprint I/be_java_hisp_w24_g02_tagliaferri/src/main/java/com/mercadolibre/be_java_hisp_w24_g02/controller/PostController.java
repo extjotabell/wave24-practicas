15773fafa1616911834fd892c1e53165fca146ac
stagliaferri <stefanoguillermo.tagliaferri@mercadolibre.com>
2024-01-18 10:04:06 -0300

    minor fix

--- 4. sprint I/be_java_hisp_w24_g02_tagliaferri/src/main/java/com/mercadolibre/be_java_hisp_w24_g02/controller/PostController.java
+++ 4. sprint I/be_java_hisp_w24_g02_tagliaferri/src/main/java/com/mercadolibre/be_java_hisp_w24_g02/controller/PostController.java
@@ -28,18 +28,35 @@ public class PostController {
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    /**
+     * Add new promo product post
+     * @param createPromoPostDTO
+     * @return ResponseEntity with status 200 or 400 if the request is invalid
+     */
+
     @PostMapping("/promo-post")
     public ResponseEntity<?> addNewPromoPost(@Valid @RequestBody CreatePromoPostDTO createPromoPostDTO){
         this.postService.createPromoPost(createPromoPostDTO);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    /**
+     * Get the numbers of promo posts by user id
+     * @param user_id
+     * @return PromoPostCountUserDTO with userId, userName and count of promo posts.
+     */
 
     @GetMapping("/promo-post/count")
     public ResponseEntity<PromoPostCountUserDTO> countPromoPostUser(@RequestParam Integer user_id){
         PromoPostCountUserDTO promoPostCountUser = postService.getPromoPostCountUser(user_id);
         return ResponseEntity.ok(promoPostCountUser);
     }
+
+    /**
+     * Get the list of promo posts by discount higher or equal to the discount param
+     * @param discount
+     * @return List of promoPostDTO
+     */
 
     @GetMapping("/promo-post/{discount}/list")
     public ResponseEntity<List<PromoPostDTO>> listPromoPostByDiscount(@PathVariable Double discount){
