d6953d578e4512fd1c29afdf38b87a36e12a1a18
victoriaborquezcarm <victoria.borquezcarmona@mercadolibre.cl>
2024-01-19 17:19:03 -0300

    [feat] Added exercise ObtenerDiploma

---
+++ 5. testing/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/ObtenerDiplomaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.meli.obtenerdiploma;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObetenerDiplomaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+}
