5a776c004e506dfdf410aca0324bd9fdcd12342b
mAnzurez <marcos.anzurezpalacios@mercadolibre.com.mx>
2024-01-19 18:12:21 -0600

    [feat] Add diploma base project

---
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/dto/ErrorDTO.java
@@ -0,0 +1,6 @@
+package com.meli.obtenerdiploma.dto;
+
+
+public record ErrorDTO(String name,
+                       String description){
+}
---
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/service/IStudentService.java
@@ -0,0 +1,13 @@
+package com.meli.obtenerdiploma.service;
+
+import com.meli.obtenerdiploma.dto.StudentDTO;
+
+import java.util.Set;
+
+public interface IStudentService {
+    boolean create(StudentDTO stu);
+    StudentDTO read(Long id);
+    boolean update(StudentDTO stu);
+    boolean delete(Long id);
+    Set<StudentDTO> getAll();
+}
