180f74ba1d97fd5a8f7b6dee202e42a38b696dc8
saenzJuan <juan.saenzgarcia@mercadolibre.com.co>
2024-01-23 13:31:46 -0500

    [FIX] fix US 0011

---
+++ 4. sprint I/be_java_hisp_w24_g02_saenz 12.49.25 p. m./src/main/java/com/mercadolibre/be_java_hisp_w24_g02/dto/UserRelationshipsDTO.java
@@ -0,0 +1,18 @@
+package com.mercadolibre.be_java_hisp_w24_g02.dto;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.mercadolibre.be_java_hisp_w24_g02.serializer.UserRelationshipsDTOSerializer;
+
+import java.util.List;
+
+@JsonSerialize(using = UserRelationshipsDTOSerializer.class)
+public record UserRelationshipsDTO(
+        Integer userId,
+        String userName,
+        List<UserBasicInfoDTO> relationshiDTOList,
+        boolean isFollowers
+
+) {
+
+
+}
---
+++ 4. sprint I/be_java_hisp_w24_g02_saenz 12.49.25 p. m./src/main/java/com/mercadolibre/be_java_hisp_w24_g02/service/interfaces/IUserService.java
@@ -0,0 +1,17 @@
+package com.mercadolibre.be_java_hisp_w24_g02.service.interfaces;
+
+import com.mercadolibre.be_java_hisp_w24_g02.dto.UserFollowedsPostsDTO;
+import com.mercadolibre.be_java_hisp_w24_g02.dto.UserFollowersCountDTO;
+import com.mercadolibre.be_java_hisp_w24_g02.dto.UpdateToRelationshipsDTO;
+import com.mercadolibre.be_java_hisp_w24_g02.dto.UserRelationshipsDTO;
+
+
+public interface IUserService {
+
+
+    UserFollowersCountDTO getUserFollowersCount(Integer userId);
+    void unfollowUser(UpdateToRelationshipsDTO followUserDTO);
+    void followUser(UpdateToRelationshipsDTO followUserDTO);
+    UserRelationshipsDTO getUserFollowers(Integer userId, String order);
+    UserRelationshipsDTO getUserFollowed(Integer userId, String order);
+}
