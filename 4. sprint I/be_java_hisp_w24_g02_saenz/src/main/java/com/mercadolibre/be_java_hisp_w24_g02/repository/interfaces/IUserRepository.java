2ea17f27b290ac09211c5edee17ef1a0ba7d3829
Juan Felipe Saenz <juan.saenzgarcia@mercadolibre.com.co>
2024-01-23 12:41:26 -0500

    Delete 4. sprint I/be_java_hisp_w24_g02_saenz directory

--- 4. sprint I/be_java_hisp_w24_g02_saenz/src/main/java/com/mercadolibre/be_java_hisp_w24_g02/repository/interfaces/IUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mercadolibre.be_java_hisp_w24_g02.repository.interfaces;
-
-import com.mercadolibre.be_java_hisp_w24_g02.entity.User;
-import com.mercadolibre.be_java_hisp_w24_g02.repository.interfaces.IDAORepository;
-
-public interface IUserRepository extends IDAORepository<User> {
-
-
-}
