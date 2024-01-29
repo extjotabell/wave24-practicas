e85330759a4db69cc4a3f27b2d89455c662c4e1f
JuanIgnacioZunino <juanignacio.zunino@mercadolibre.com>
2024-01-29 11:47:05 -0300

    el coverage llego a 92

--- 6. sprint II/Sprint individual 2/be_java_hisp_w24_g05/src/test/java/org/be_java_hisp_w24_g05/integration/ProductControllerTest.java
+++ 6. sprint II/Sprint individual 2/be_java_hisp_w24_g05/src/test/java/org/be_java_hisp_w24_g05/integration/ProductControllerTest.java
@@ -22,6 +22,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -66,7 +67,7 @@ public class ProductControllerTest {
 
         // ejecuta la request con una url provista
         mockMvc.perform(request)
-                .andDo(MockMvcResultHandlers.print())
+                .andDo(print())
                 //.andExpect(statusExpected) // verifica el status
                 //.andExpect(bodyExpected) // verifica el body
                 //.andExpect(contentTypeExpected); // verifica el tipo
@@ -75,7 +76,39 @@ public class ProductControllerTest {
     }
 
     @Test
-    public void testCountUserFollowers() throws Exception {
+    public void recentPostsOfFollowedUsersNotFoundUser() throws Exception {
+        // Arrange
+
+        // paso 1 - request
+        String url = "/products/followed/{userId}/list";
+        Integer param = 10;
+        RequestBuilder request = get(url, param);
+
+        // paso 2 - construir el expected del status
+        ResultMatcher statusExpected = status().isNotFound();
+
+        // paso 3 - construir el expected body
+
+
+        ExceptionDto exceptionDto = new ExceptionDto("User not found");
+        ResultMatcher bodyExpected = content().json(
+                mapper.writeValueAsString(exceptionDto)
+        );
+
+        // paso 4 - constuir el expected content type
+
+        ResultMatcher contentTypeExpected = content().contentType(MediaType.APPLICATION_JSON);
+
+        // act & assert
+
+        // ejecuta la request con una url provista
+        mockMvc.perform(request)
+                .andDo(print())
+                .andExpectAll(statusExpected, bodyExpected, contentTypeExpected);
+    }
+
+    @Test
+    public void testCountUserFollowersPositive() throws Exception {
 
         String url = "/users";
         RequestBuilder request = get(url + "/1/followers/count");
@@ -90,13 +123,13 @@ public class ProductControllerTest {
         ResultMatcher bodyExpected = content().json(mapper.writeValueAsString(countFollowersDto));
         ResultMatcher contentExpected = content().contentType(MediaType.APPLICATION_JSON);
         mockMvc.perform(request)
-                .andDo(MockMvcResultHandlers.print())
+                .andDo(print())
                 .andExpectAll(statusExpected, bodyExpected, contentExpected);
 
     }
 
     @Test
-    public void testFollowUserHappyPath() throws Exception {
+    public void testFollowUserPositive() throws Exception {
         String url = "/users/{userId}/follow/{userIdToFollow}";
         String userId = "3";
         String userIdToFollow = "4";
@@ -116,7 +149,58 @@ public class ProductControllerTest {
     }
 
     @Test
-    public void userFollowersTest() throws Exception {
+    public void testFollowUserNegative() throws Exception {
+        String url = "/users/{userId}/follow/{userIdToFollow}";
+        String userId = "3";
+        String userIdToFollow = "3";
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post(url, userId, userIdToFollow);
+        ResultMatcher expectedStatus = status().isBadRequest();
+        ResultMatcher expectedContentType = content().contentType(MediaType.APPLICATION_JSON);
+        mockMvc.perform(requestBuilder)
+                .andExpect(expectedStatus)
+                .andExpect(expectedContentType);
+    }
+
+    @Test
+    public void testFollowUserIncorrectUser() throws Exception {
+        String url = "/users/{userId}/follow/{userIdToFollow}";
+        String userId = "1";
+        String userIdToFollow = "999";
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post(url, userId, userIdToFollow);
+
+        ResultMatcher expectedStatus = status().isBadRequest();
+
+        ResultMatcher expectedContentType = content().contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(expectedStatus)
+                .andExpect(expectedContentType);
+    }
+
+    @Test
+    public void testUnfollowUserPositive() throws Exception {
+        String url = "/users/{userId}/unfollow/{userIdToUnfollow}";
+        String userId = "3";
+        String userIdToUnfollow = "4";
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post(url, userId, userIdToUnfollow);
+        ResultMatcher expectedStatus = status().isOk();
+        UserFollowedDto userFollowedDto = new UserFollowedDto(
+                3,
+                "User3",
+                0
+        );
+        ResultMatcher expectedBody = content().json(mapper.writeValueAsString(userFollowedDto));
+        ResultMatcher expectedContentType = content().contentType(MediaType.APPLICATION_JSON);
+        mockMvc.perform(requestBuilder)
+                .andExpect(expectedStatus)
+                .andExpect(expectedBody)
+                .andExpect(expectedContentType);
+    }
+
+    @Test
+    public void userFollowersTestPositive() throws Exception {
         String url = "/users/{userId}/followers/list";
         String userId = "1";
 
@@ -135,5 +219,30 @@ public class ProductControllerTest {
                 .andExpect(expectedBody)
                 .andExpect(expectedContentType);
     }
+
+    @Test
+    public void addPostHappyPath() throws Exception {
+        String url = "/products/post";
+        PostDto postDto = new PostDto(1, "26-01-2024", new ProductDto(
+                1, "Teclado Gamer", "Gamer", "Racer", "Black", "Special Edition"), 100, 1500.50);
+
+        RequestBuilder request = MockMvcRequestBuilders.post(url)
+                .content(
+                        mapper.writeValueAsString(postDto)
+                )
+                .contentType(MediaType.APPLICATION_JSON);
+        ResultMatcher statusExpected = MockMvcResultMatchers.status().isOk();
+
+        ResultMatcher bodyExpected = MockMvcResultMatchers.content().json(
+                mapper.writeValueAsString(postDto)
+        );
+        ResultMatcher contentTypeExpected = MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpectAll(statusExpected, bodyExpected, contentTypeExpected);
+    }
+
+
 }
 
