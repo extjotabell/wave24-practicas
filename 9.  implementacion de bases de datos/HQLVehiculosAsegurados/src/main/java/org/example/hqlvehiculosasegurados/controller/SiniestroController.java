d59ce7240ec6cd5f0dc19c3ec914c8c5d2107945
victoriaborquezcarm <victoria.borquezcarmona@mercadolibre.cl>
2024-02-12 15:12:23 -0300

    [feat]: Added SiniestroController and VehiculoController with enpoint for crud operations (WIP)

---
+++ 9.  implementacion de bases de datos/HQLVehiculosAsegurados/src/main/java/org/example/hqlvehiculosasegurados/controller/SiniestroController.java
@@ -0,0 +1,46 @@
+package org.example.hqlvehiculosasegurados.controller;
+
+import org.example.hqlvehiculosasegurados.dto.ResponseDto;
+import org.example.hqlvehiculosasegurados.dto.SiniestroDto;
+import org.example.hqlvehiculosasegurados.service.SiniestroService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/Siniestros")
+public class SiniestroController {
+
+    private final SiniestroService siniestroService;
+
+    public SiniestroController(SiniestroService siniestroService) {
+        this.siniestroService = siniestroService;
+    }
+
+    @PostMapping
+    public ResponseEntity<SiniestroDto> saveSiniestro(SiniestroDto siniestroDto){
+        return new ResponseEntity<>(siniestroService.save(siniestroDto), HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<SiniestroDto>> getAllSiniestros(){
+        return new ResponseEntity<>(siniestroService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SiniestroDto> getSiniestroById(@PathVariable Long id){
+        return new ResponseEntity<>(siniestroService.getById(id), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseDto> updateSiniestroById(@PathVariable Long id, @RequestBody SiniestroDto siniestroDto){
+        return new ResponseEntity<>(siniestroService.update(id, siniestroDto), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseDto> deleteSiniestroById(@PathVariable Long id){
+        return new ResponseEntity<>(siniestroService.deleteById(id), HttpStatus.OK);
+    }
+}
---
+++ 9.  implementacion de bases de datos/HQLVehiculosAsegurados/src/main/java/org/example/hqlvehiculosasegurados/controller/VehiculoController.java
@@ -0,0 +1,54 @@
+package org.example.hqlvehiculosasegurados.controller;
+
+import org.example.hqlvehiculosasegurados.dto.PatenteDto;
+import org.example.hqlvehiculosasegurados.dto.ResponseDto;
+import org.example.hqlvehiculosasegurados.dto.VehiculoDto;
+import org.example.hqlvehiculosasegurados.service.VehiculoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class VehiculoController {
+
+    private final VehiculoService vehiculoService;
+
+    public VehiculoController(VehiculoService vehiculoService) {
+        this.vehiculoService = vehiculoService;
+    }
+
+    @PostMapping("/vehiculos")
+    public ResponseEntity<VehiculoDto> saveVehiculo(VehiculoDto vehiculoDto){
+        return new ResponseEntity<>(vehiculoService.save(vehiculoDto), HttpStatus.OK);
+    }
+
+    @GetMapping("/vehiculos/all")
+    public ResponseEntity<List<VehiculoDto>> getAllVehiculos(){
+        return new ResponseEntity<>(vehiculoService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/vehiculos/{id}")
+    public ResponseEntity<VehiculoDto> getVehiculosById(@PathVariable Long id){
+        return new ResponseEntity<>(vehiculoService.getById(id), HttpStatus.OK);
+    }
+
+    @PutMapping("/vehiculos/{id}")
+    public ResponseEntity<ResponseDto> updateVehiculoById(@PathVariable Long id, @RequestBody VehiculoDto vehiculoDto){
+        return new ResponseEntity<>(vehiculoService.update(id, vehiculoDto), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/vehiculos/{id}")
+    public ResponseEntity<ResponseDto> deleteVehiculoById(@PathVariable Long id){
+        return new ResponseEntity<>(vehiculoService.deleteById(id), HttpStatus.OK);
+    }
+
+
+
+    @GetMapping("/patentes")
+    public ResponseEntity<PatenteDto> getAllPatentes(){
+        return new ResponseEntity<>(vehiculoService.getAllPatentes(), HttpStatus.OK);
+    }
+}
