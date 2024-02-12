978ee756be1124fcb279de097c153403465ee9bb
victoriaborquezcarm <victoria.borquezcarmona@mercadolibre.cl>
2024-02-12 11:59:56 -0300

    [feat]: Added findTestCaseUpdatedAfterDate endpoint

--- 9.  implementacion de bases de datos/QATester/src/main/java/org/example/qatester/controller/TestCaseController.java
+++ 9.  implementacion de bases de datos/QATester/src/main/java/org/example/qatester/controller/TestCaseController.java
@@ -3,6 +3,7 @@ package org.example.qatester.controller;
 import org.example.qatester.dto.ResponseDto;
 import org.example.qatester.dto.TestCaseDto;
 import org.example.qatester.service.TestCaseService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/testcases")
 public class TestCaseController {
 
     private final TestCaseService testCaseService;
@@ -19,33 +21,33 @@ public class TestCaseController {
         this.testCaseService = testCaseService;
     }
 
-    @PostMapping("/api/testcases/new")
+    @PostMapping("/new")
     public ResponseEntity<TestCaseDto> saveTestCase(@RequestBody TestCaseDto testCaseDto){
         return new ResponseEntity<>(testCaseService.saveTestCase(testCaseDto), HttpStatus.OK);
     }
 
-    @GetMapping("/api/testcases")
+    @GetMapping
     public ResponseEntity<List<TestCaseDto>> getAllTestCase(){
         return new ResponseEntity<>(testCaseService.getAllTestCase(), HttpStatus.OK);
     }
 
-    @GetMapping("/api/testcases/{id}")
+    @GetMapping("{id}")
     public ResponseEntity<TestCaseDto> getTestCaseById(@PathVariable Long id){
         return new ResponseEntity<>(testCaseService.getTestCaseById(id), HttpStatus.OK);
     }
 
-    @PutMapping("/api/testcases/{id}")
+    @PutMapping("{id}")
     public ResponseEntity<TestCaseDto> updateTestCaseById(@PathVariable Long id, @RequestBody TestCaseDto testCaseDto){
         return new ResponseEntity<>(testCaseService.updateTestCase(id, testCaseDto), HttpStatus.OK);
     }
 
-    @DeleteMapping("/api/testcases/{id}")
+    @DeleteMapping("{id}")
     public ResponseEntity<ResponseDto> deleteTestCaseById(@PathVariable Long id){
         return new ResponseEntity<>(testCaseService.deleteTestCaseById(id), HttpStatus.OK);
     }
 
-    @GetMapping("/api/testcases/")
-    public ResponseEntity<List<TestCaseDto>> findTestCaseUpdatedAfterDate(@RequestParam("last_update") LocalDate date){
+    @GetMapping("/")
+    public ResponseEntity<List<TestCaseDto>> findTestCaseUpdatedAfterDate(@RequestParam("last_update") @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate date){
         return new ResponseEntity<>(testCaseService.findTestCaseUpdatedAfterDate(date), HttpStatus.OK);
     }
 }
