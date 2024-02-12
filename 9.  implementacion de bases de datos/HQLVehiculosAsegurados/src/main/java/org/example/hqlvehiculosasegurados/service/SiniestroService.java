e1c3501335edb108b1796113710effb0fa0b6837
victoriaborquezcarm <victoria.borquezcarmona@mercadolibre.cl>
2024-02-12 15:05:53 -0300

    [feat]: Added SiniestroService with crud operations in service pkg

--- 9.  implementacion de bases de datos/HQLVehiculosAsegurados/src/main/java/org/example/hqlvehiculosasegurados/service/SiniestroService.java
+++ 9.  implementacion de bases de datos/HQLVehiculosAsegurados/src/main/java/org/example/hqlvehiculosasegurados/service/SiniestroService.java
@@ -2,6 +2,8 @@ package org.example.hqlvehiculosasegurados.service;
 
 import org.example.hqlvehiculosasegurados.dto.ResponseDto;
 import org.example.hqlvehiculosasegurados.dto.SiniestroDto;
+import org.example.hqlvehiculosasegurados.entity.Siniestro;
+import org.example.hqlvehiculosasegurados.mapper.Mapper;
 import org.example.hqlvehiculosasegurados.repository.ISiniestroRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,32 +14,43 @@ public class SiniestroService implements ISiniestroService{
 
     private final ISiniestroRepository siniestroRepository;
 
-    public SiniestroService(ISiniestroRepository siniestroRepository) {
+    private final Mapper mapper;
+
+    public SiniestroService(ISiniestroRepository siniestroRepository, Mapper mapper) {
         this.siniestroRepository = siniestroRepository;
+        this.mapper = mapper;
     }
 
     @Override
     public SiniestroDto save(SiniestroDto siniestroDto) {
-        return null;
+        Siniestro siniestro = mapper.convertDtoToSiniestro(siniestroDto);
+        siniestroRepository.save(siniestro);
+        return mapper.convertSiniestroToDto(siniestro);
     }
 
     @Override
     public List<SiniestroDto> getAll() {
-        return null;
+        return mapper.convertListSiniestroToListDto(siniestroRepository.findAll());
     }
 
     @Override
-    public SiniestroDto getById(Long aLong) {
-        return null;
+    public SiniestroDto getById(Long id) {
+        return mapper.convertSiniestroToDto(siniestroRepository.findById(id).orElseThrow());
     }
 
     @Override
-    public ResponseDto update(Long aLong, SiniestroDto siniestroDto) {
-        return null;
+    public ResponseDto update(Long id, SiniestroDto siniestroDto) {
+        Siniestro siniestroToUpdate = siniestroRepository.getById(id);
+        siniestroToUpdate.setId(siniestroDto.id());
+        siniestroToUpdate.setFechaSiniestro(siniestroDto.fechaSiniestro());
+        siniestroToUpdate.setPerdidaEconomica(siniestroDto.perdidaEconomica());
+        return new ResponseDto("Siniestro modificado");
     }
 
     @Override
-    public ResponseDto deleteById(Long aLong) {
-        return null;
+    public ResponseDto deleteById(Long id) {
+        Siniestro siniestro = siniestroRepository.getById(id);
+        siniestroRepository.delete(siniestro);
+        return new ResponseDto("Siniestro eliminado");
     }
 }
