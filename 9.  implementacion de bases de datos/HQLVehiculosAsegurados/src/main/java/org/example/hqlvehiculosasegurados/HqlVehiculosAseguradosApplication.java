c047ee6ee4f94911a2f0954285a14bb55d59d5f1
victoriaborquezcarm <victoria.borquezcarmona@mercadolibre.cl>
2024-02-12 12:08:42 -0300

    [feat]: Added exercise HQLVehiculosAsegurados

---
+++ 9.  implementacion de bases de datos/HQLVehiculosAsegurados/src/main/java/org/example/hqlvehiculosasegurados/HqlVehiculosAseguradosApplication.java
@@ -0,0 +1,13 @@
+package org.example.hqlvehiculosasegurados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HqlVehiculosAseguradosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HqlVehiculosAseguradosApplication.class, args);
+    }
+
+}
---
+++ 9.  implementacion de bases de datos/HQLVehiculosAsegurados/src/test/java/org/example/hqlvehiculosasegurados/HqlVehiculosAseguradosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hqlvehiculosasegurados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HqlVehiculosAseguradosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
