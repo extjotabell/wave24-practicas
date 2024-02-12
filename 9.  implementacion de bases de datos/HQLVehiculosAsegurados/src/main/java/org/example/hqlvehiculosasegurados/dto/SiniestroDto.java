e36cdd2dbfdcd0818ae683fb3528a4fa88fed8f2
victoriaborquezcarm <victoria.borquezcarmona@mercadolibre.cl>
2024-02-12 12:30:00 -0300

    [feat]: Added VehiculoDto and SiniestroDto in dto pkg

---
+++ 9.  implementacion de bases de datos/HQLVehiculosAsegurados/src/main/java/org/example/hqlvehiculosasegurados/dto/SiniestroDto.java
@@ -0,0 +1,16 @@
+package org.example.hqlvehiculosasegurados.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.example.hqlvehiculosasegurados.entity.Vehiculo;
+
+import java.time.LocalDate;
+
+public record SiniestroDto(
+        Long id,
+        @JsonProperty("fecha_siniestro")
+        LocalDate fechaSiniestro,
+        @JsonProperty("perdida_economica")
+        Double perdidaEconomica,
+        Vehiculo vehiculo
+) {
+}
---
+++ 9.  implementacion de bases de datos/HQLVehiculosAsegurados/src/main/java/org/example/hqlvehiculosasegurados/dto/VehiculoDto.java
@@ -0,0 +1,20 @@
+package org.example.hqlvehiculosasegurados.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.example.hqlvehiculosasegurados.entity.Siniestro;
+
+import java.util.List;
+
+public record VehiculoDto(
+        Long id,
+        String patente,
+        String marca,
+        String modelo,
+
+        @JsonProperty("anio_fabricacion")
+        Integer anioFabricacion,
+        @JsonProperty("cantidad_de_ruedas")
+        Integer cantidadDeRuedas,
+        List<Siniestro> siniestros
+) {
+}
