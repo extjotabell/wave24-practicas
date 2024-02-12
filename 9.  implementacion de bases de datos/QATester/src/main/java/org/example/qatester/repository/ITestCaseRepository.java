d8ee26f8433e910d113b61c1ba75aed609c54cb6
victoriaborquezcarm <victoria.borquezcarmona@mercadolibre.cl>
2024-02-12 11:58:25 -0300

    [feat]: Added findByLastUpdateAfter method like a query in repository

--- 9.  implementacion de bases de datos/QATester/src/main/java/org/example/qatester/repository/ITestCaseRepository.java
+++ 9.  implementacion de bases de datos/QATester/src/main/java/org/example/qatester/repository/ITestCaseRepository.java
@@ -2,6 +2,16 @@ package org.example.qatester.repository;
 
 import org.example.qatester.entity.TestCase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
 public interface ITestCaseRepository extends JpaRepository<TestCase, Long> {
+
+    @Query("FROM TestCase tc WHERE tc.lastUpdate > :last_update")
+    List<TestCase> findByLastUpdateAfter(@Param("last_update") LocalDate last_update);
 }
