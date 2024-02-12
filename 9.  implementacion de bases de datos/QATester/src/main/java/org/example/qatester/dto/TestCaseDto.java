f09c2c8496880ecaef02ac720a4e6c41ff50bb31
victoriaborquezcarm <victoria.borquezcarmona@mercadolibre.cl>
2024-02-12 11:17:46 -0300

    [feat]: Modified TestCaseDto
    Added JsonProperty in attributes and JsonFormat en lastUpdate attribute

--- 9.  implementacion de bases de datos/QATester/src/main/java/org/example/qatester/dto/TestCaseDto.java
+++ 9.  implementacion de bases de datos/QATester/src/main/java/org/example/qatester/dto/TestCaseDto.java
@@ -1,13 +1,26 @@
 package org.example.qatester.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+
 import java.time.LocalDate;
 
 public record TestCaseDto(
+        @JsonProperty("id_case")
         Long idCase,
         String description,
         Boolean tested,
         Boolean passed,
+        @JsonProperty("number_of_tries")
         Integer numberOfTries,
+        @JsonProperty("last_update")
+        @JsonFormat(pattern = "dd/MM/yyyy")
+        @JsonDeserialize(using = LocalDateDeserializer.class)
+        @JsonSerialize(using = LocalDateSerializer.class)
         LocalDate lastUpdate
 ) {
 }
