74cc795afb5e2f91def9656661d4136cc3bdcbf6
victoriaborquezcarm <victoria.borquezcarmona@mercadolibre.cl>
2024-02-13 09:42:23 -0300

    [feat]: Added class Mapper to convert dto-entity en mapper pkg

---
+++ 9.  implementacion de bases de datos/ObrasLiterarias/src/main/java/org/example/obrasliterarias/mapper/Mapper.java
@@ -0,0 +1,31 @@
+package org.example.obrasliterarias.mapper;
+
+import org.example.obrasliterarias.dto.ObraDto;
+import org.example.obrasliterarias.entity.Obra;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mapper {
+
+    public Obra convertDtoToEntity(ObraDto obraDto){
+        return new Obra(
+                obraDto.id(),
+                obraDto.nombre(),
+                obraDto.autor(),
+                obraDto.cantidadPaginas(),
+                obraDto.editorial(),
+                obraDto.anioPublicacion()
+        );
+    }
+
+    public ObraDto convertEntityToDto(Obra obra){
+        return new ObraDto(
+                obra.getId(),
+                obra.getNombre(),
+                obra.getAutor(),
+                obra.getCantidadPaginas(),
+                obra.getEditorial(),
+                obra.getAnioPublicacion()
+        );
+    }
+}
